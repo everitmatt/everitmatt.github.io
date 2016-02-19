f994f9eb5ccfeaf0ebd5b960a1bdffa6e5db1339
everitmatt <matt.everitt@hotmail.co.nz>
2016-02-19 13:04:57 +1300

    latest app

--- _site/local-sources/global-swell-emitters.js
+++ _site/local-sources/global-swell-emitters.js
@@ -1,115 +1,27 @@
 var swellEmitters = [
   {
-    "FIELD6": 3597910.726777897,
-    "FIELD3": -148.55467224121094,
-    "FIELD2": 41.376800537109375,
-    "FIELD5": 248,
-    "FIELD4": 5,
-    "FIELD1": "East North Pacific Ocean"
+    "FIELD6": 1322684.693073253,
+    "FIELD3": 169.4,
+    "FIELD2": -38.660316467285156,
+    "FIELD5": 30,
+    "FIELD4": 2,
+    "FIELD1": "East Tasman Sea"
   },
   {
-    "FIELD6": 4933896.781052535,
-    "FIELD3": -111.99217224121094,
-    "FIELD2": -22.268766403198242,
-    "FIELD5": 274,
-    "FIELD4": 5,
-    "FIELD1": "East South Pacific Ocean"
-  },
-  {
-    "FIELD6": 4187766.89305321,
-    "FIELD3": -41.85546112060547,
-    "FIELD2": 37.718589782714844,
-    "FIELD5": 274,
-    "FIELD4": 4,
-    "FIELD1": "North Atlantic Ocean"
-  },
-  {
-    "FIELD6": 4784344.682813717,
-    "FIELD3": -13.906243324279785,
-    "FIELD2": -34.597042083740234,
-    "FIELD5": 301,
-    "FIELD4": 4,
-    "FIELD1": "South Atlantic Ocean"
-  },
-  {
-    "FIELD6": 1120455.3709804034,
-    "FIELD3": -8.457024574279785,
-    "FIELD2": 65.29345703125,
-    "FIELD5": 138,
+    "FIELD6": 938137.8952179158,
+    "FIELD3": 117.9296875,
+    "FIELD2": -0.703113317489624,
+    "FIELD5": 35,
     "FIELD4": 3,
-    "FIELD1": "Norwegian Sea"
+    "FIELD1": "Java Sea"
   },
   {
-    "FIELD6": 581424.5723743308,
-    "FIELD3": 5.07813024520874,
-    "FIELD2": 55.07836151123047,
-    "FIELD5": 52,
-    "FIELD4": 1,
-    "FIELD1": "North Sea"
-  },
-  {
-    "FIELD6": 968343.7367807939,
-    "FIELD3": 8.945317268371582,
-    "FIELD2": 39.232242584228516,
-    "FIELD5": 64,
-    "FIELD4": 1,
-    "FIELD1": "West Mediterranean Sea"
-  },
-  {
-    "FIELD6": 1035823.1913648824,
-    "FIELD3": 26.523441314697266,
-    "FIELD2": 32.69486618041992,
-    "FIELD5": 63,
-    "FIELD4": 1,
-    "FIELD1": "East Mediterranean Sea"
-  },
-  {
-    "FIELD6": 1641713.2589441913,
-    "FIELD3": 46.738285064697266,
-    "FIELD2": 17.30868148803711,
-    "FIELD5": 88,
-    "FIELD4": 1,
-    "FIELD1": "Persian Gulf"
-  },
-  {
-    "FIELD6": 1122481.1162027908,
-    "FIELD3": 90.5078125,
-    "FIELD2": 13.239945411682129,
-    "FIELD5": 59,
-    "FIELD4": 1,
-    "FIELD1": "Bay of Bengal"
-  },
-  {
-    "FIELD6": 4507865.274447578,
-    "FIELD3": 79.2578125,
-    "FIELD2": -23.72501564025879,
-    "FIELD5": 253,
+    "FIELD6": 756547.38685265,
+    "FIELD3": -78.59374237060547,
+    "FIELD2": 14.604843139648438,
+    "FIELD5": 40,
     "FIELD4": 3,
-    "FIELD1": "Indian Ocean"
-  },
-  {
-    "FIELD6": 2486284.60922623,
-    "FIELD3": 48.49609375,
-    "FIELD2": -50.289337158203125,
-    "FIELD5": 201,
-    "FIELD4": 4,
-    "FIELD1": "West Southern Ocean"
-  },
-  {
-    "FIELD6": 1780031.994809403,
-    "FIELD3": 132.51953125,
-    "FIELD2": -47.75409698486328,
-    "FIELD5": 136,
-    "FIELD4": 4,
-    "FIELD1": "East Southern Ocean"
-  },
-  {
-    "FIELD6": 1732634.3567251775,
-    "FIELD3": 114.60614776611328,
-    "FIELD2": 15.248031616210938,
-    "FIELD5": 92,
-    "FIELD4": 3,
-    "FIELD1": "South China Sea"
+    "FIELD1": "Caribbean Sea"
   },
   {
     "FIELD6": 784328.042863558,
@@ -123,41 +35,65 @@ var swellEmitters = [
     "FIELD6": 838080.6822885339,
     "FIELD3": 131.81640625,
     "FIELD2": 42.29356002807617,
-    "FIELD5": 58,
+    "FIELD5": 50,
     "FIELD4": 1,
     "FIELD1": "Sea of Japan"
   },
   {
-    "FIELD6": 1369163.7931645098,
-    "FIELD3": 155.84402465820312,
-    "FIELD2": -19.441648483276367,
-    "FIELD5": 55,
-    "FIELD4": 2,
-    "FIELD1": "Coral Sea"
+    "FIELD6": 581424.5723743308,
+    "FIELD3": 5.07813024520874,
+    "FIELD2": 55.07836151123047,
+    "FIELD5": 52,
+    "FIELD4": 1,
+    "FIELD1": "North Sea"
+  },
+  {
+    "FIELD6": 948219.0011230985,
+    "FIELD3": -89.84374237060547,
+    "FIELD2": 23.72500991821289,
+    "FIELD5": 53,
+    "FIELD4": 3,
+    "FIELD1": "Gulf of Mexico"
+  },
+  {
+    "FIELD6": 1122481.1162027908,
+    "FIELD3": 90.5078125,
+    "FIELD2": 13.239945411682129,
+    "FIELD5": 58,
+    "FIELD4": 1,
+    "FIELD1": "Bay of Bengal"
+  },
+  {
+    "FIELD6": 968343.7367807939,
+    "FIELD3": 8.945317268371582,
+    "FIELD2": 39.232242584228516,
+    "FIELD5": 59,
+    "FIELD4": 1,
+    "FIELD1": "West Mediterranean Sea"
+  },
+  {
+    "FIELD6": 1120455.3709804034,
+    "FIELD3": -8.457024574279785,
+    "FIELD2": 65.29345703125,
+    "FIELD5": 60,
+    "FIELD4": 3,
+    "FIELD1": "Norwegian Sea"
+  },
+  {
+    "FIELD6": 1035823.1913648824,
+    "FIELD3": 26.523441314697266,
+    "FIELD2": 32.69486618041992,
+    "FIELD5": 63,
+    "FIELD4": 1,
+    "FIELD1": "East Mediterranean Sea"
   },
   {
     "FIELD6": 1322684.693073253,
     "FIELD3": 160.765625,
-    "FIELD2": -37.160316467285156,
-    "FIELD5": 60,
+    "FIELD2": -35.160316467285156,
+    "FIELD5": 65,
     "FIELD4": 2,
-    "FIELD1": "Tasman Sea"
-  },
-  {
-    "FIELD6": 4204501.12978141,
-    "FIELD3": 142.36328125,
-    "FIELD2": 25.324161529541016,
-    "FIELD5": 255,
-    "FIELD4": 5,
-    "FIELD1": "West North Pacific Ocean"
-  },
-  {
-    "FIELD6": 2384608.969546679,
-    "FIELD3": 180,
-    "FIELD2": -40.84706115722656,
-    "FIELD5": 162,
-    "FIELD4": 5,
-    "FIELD1": "West South Pacific Ocean"
+    "FIELD1": "West Tasman Sea"
   },
   {
     "FIELD6": 1117438.404243476,
@@ -176,27 +112,99 @@ var swellEmitters = [
     "FIELD1": "Baltic Sea"
   },
   {
-    "FIELD6": 938137.8952179158,
-    "FIELD3": 117.9296875,
-    "FIELD2": -0.703113317489624,
-    "FIELD5": 35,
+    "FIELD6": 1732634.3567251775,
+    "FIELD3": 114.60614776611328,
+    "FIELD2": 15.248031616210938,
+    "FIELD5": 82,
     "FIELD4": 3,
-    "FIELD1": "Java Sea"
+    "FIELD1": "South China Sea"
   },
   {
-    "FIELD6": 948219.0011230985,
-    "FIELD3": -89.84374237060547,
-    "FIELD2": 23.72500991821289,
-    "FIELD5": 53,
-    "FIELD4": 3,
-    "FIELD1": "Gulf of Mexico"
+    "FIELD6": 1641713.2589441913,
+    "FIELD3": 46.738285064697266,
+    "FIELD2": 17.30868148803711,
+    "FIELD5": 88,
+    "FIELD4": 1,
+    "FIELD1": "Persian Gulf"
   },
   {
-    "FIELD6": 756547.38685265,
-    "FIELD3": -78.59374237060547,
-    "FIELD2": 14.604843139648438,
-    "FIELD5": 40,
+    "FIELD6": 1369163.7931645098,
+    "FIELD3": 172.84402465820312,
+    "FIELD2": -21.041648483276367,
+    "FIELD5": 94,
+    "FIELD4": 2,
+    "FIELD1": "Pacific Islands"
+  },
+  {
+    "FIELD6": 1780031.994809403,
+    "FIELD3": 132.51953125,
+    "FIELD2": -47.75409698486328,
+    "FIELD5": 116,
+    "FIELD4": 4,
+    "FIELD1": "East Southern Ocean"
+  },
+  {
+    "FIELD6": 2384608.969546679,
+    "FIELD3": 190,
+    "FIELD2": -45.84706115722656,
+    "FIELD5": 172,
+    "FIELD4": 5,
+    "FIELD1": "West South Pacific Ocean"
+  },
+  {
+    "FIELD6": 2486284.60922623,
+    "FIELD3": 48.49609375,
+    "FIELD2": -50.289337158203125,
+    "FIELD5": 180,
+    "FIELD4": 4,
+    "FIELD1": "West Southern Ocean"
+  },
+  {
+    "FIELD6": 4507865.274447578,
+    "FIELD3": 79.2578125,
+    "FIELD2": -23.72501564025879,
+    "FIELD5": 253,
     "FIELD4": 3,
-    "FIELD1": "Caribbean Sea"
-  }
+    "FIELD1": "Indian Ocean"
+  },
+  {
+    "FIELD6": 4204501.12978141,
+    "FIELD3": 162.36328125,
+    "FIELD2": 25.324161529541016,
+    "FIELD5": 255,
+    "FIELD4": 5,
+    "FIELD1": "West North Pacific Ocean"
+  },
+  {
+    "FIELD6": 4784344.682813717,
+    "FIELD3": -13.906243324279785,
+    "FIELD2": -54.597042083740234,
+    "FIELD5": 273,
+    "FIELD4": 4,
+    "FIELD1": "South Atlantic Ocean"
+  },
+  {
+    "FIELD6": 4187766.89305321,
+    "FIELD3": -41.85546112060547,
+    "FIELD2": 45.718589782714844,
+    "FIELD5": 274,
+    "FIELD4": 4,
+    "FIELD1": "North Atlantic Ocean"
+  },
+  {
+    "FIELD6": 4933896.781052535,
+    "FIELD3": -111.99217224121094,
+    "FIELD2": -45.268766403198242,
+    "FIELD5": 300,
+    "FIELD4": 5,
+    "FIELD1": "East South Pacific Ocean"
+  },
+  {
+    "FIELD6": 3597910.726777897,
+    "FIELD3": -148.55467224121094,
+    "FIELD2": 41.376800537109375,
+    "FIELD5": 400,
+    "FIELD4": 5,
+    "FIELD1": "East North Pacific Ocean"
+  },
 ]
